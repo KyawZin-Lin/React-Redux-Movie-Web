a72962eebc088fa4250f7d00a318d1932e4b9b42
JueMay <juejuemaymm@gmail.com>
2024-04-01 16:00:36 +0630

    Spinner Added In movie List

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 import { useSelector } from "react-redux";
+import { Spinner } from "flowbite-react";
 
 const Movie = () => {
-
   let movies = [];
-  movies = useSelector((state)=>state.movies.movies);
-  console.log(movies)
+  movies = useSelector((state) => state.movies.movies);
+  console.log(movies);
   return (
-  <div className="container mx-auto mt-6 p-3">
+    <div className="container mx-auto mt-6 p-3">
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
-       {
-        movies.length > 0 ?   movies.map((movie)=>{
-          return (
-            <MovieCard movie={movie} key={movie.id}/>
-          )
-        }) : <h1>
-          there is no movies
-        </h1>
-       }
-        
+        {movies.length > 0 ? (
+          movies.map((movie) => {
+            return <MovieCard movie={movie} key={movie.id} />;
+          })
+        ) : (
+          <div className="text-center">
+            <h1>
+              <Spinner aria-label="Medium sized spinner example" size="md" />
+            </h1>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
   );
 };
 
