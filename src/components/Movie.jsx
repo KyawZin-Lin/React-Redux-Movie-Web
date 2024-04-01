af2cfc5938e51aa060d33c067620f497b347dccc
JueMay <juejuemaymm@gmail.com>
2024-04-01 15:54:23 +0630

    Movie Details Api Done

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import { useSelector } from "react-redux";
 
 const Movie = () => {
+
+  let movies = [];
+  movies = useSelector((state)=>state.movies.movies);
+  console.log(movies)
   return (
-  <div className="container mx-auto mt-6">
+  <div className="container mx-auto mt-6 p-3">
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
-        <MovieCard/>
+       {
+        movies.length > 0 ?   movies.map((movie)=>{
+          return (
+            <MovieCard movie={movie} key={movie.id}/>
+          )
+        }) : <h1>
+          there is no movies
+        </h1>
+       }
+        
     </div>
   </div>
   );
